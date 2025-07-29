8c015534e58fda8fd4b065dd2e7c047e466ff43e
AkatsukiThrowaway <168493142+AkatsukiThrowaway@users.noreply.github.com>
2025-07-29 00:42:44 -0400

    Reorder, label modify_avg.py

--- generate_scripts/modify_avg.py
+++ generate_scripts/modify_avg.py
@@ -83,14 +83,21 @@ def make_edits(script_json, avgID):
 ###########################################################################
 # Purely corrective changes - no real reason to ever need to undo these
 ###########################################################################
-	#Fix unset charPos
+
+#Fix unset charPos
+
+	#詐欺事件の調査（1） 4/6
 	if avgID == '10008':
 		script_json['dialogueFrames'][98]['charPos'] = 1
 		script_json['dialogueFrames'][98]['character']['mirror'] = 1
+	
+	#元マフィアの隠れ家を再捜索 1/4
 	elif avgID == '12766':
 		script_json['dialogueFrames'][16]['charPos'] = 1
 
-	#Apply corrections to script, found in mobile version files
+#Apply corrections to script, found in mobile version files
+
+	#Both of these are 防空訓練(2) 7/7
 	elif avgID == '10330':
 		script_json['dialogueFrames'][85]['strID'] = 1130996
 		script_json['dialogueFrames'][115]['strID'] = 1130966
@@ -100,10 +107,14 @@ def make_edits(script_json, avgID):
 		script_json['dialogueFrames'][88]['character']['speaker'] = 4
 		script_json['dialogueFrames'][118]['strID'] = 1130966
 	
-	#The CharFadeOut was on the wrong line
+#CharFadeOut was set on the wrong line
+
+	#「絵空の記憶」　8/12
 	elif avgID == '1138':
 		script_json['dialogueFrames'][108]['CharFadeOut'] = 1
 		script_json['dialogueFrames'][109]['CharFadeOut'] = 0
+		
+	#Bracer’s kitchen! 後日談
 	elif avgID == '1209':
 		script_json['dialogueFrames'][14]['CharFadeOut'] = 1
 		script_json['dialogueFrames'][15]['CharFadeOut'] = 0
@@ -111,6 +122,29 @@ def make_edits(script_json, avgID):
 ######################################################################################
 # Subjective changes - up to interpretation, someone may want to change or undo these
 ######################################################################################
+
+	#クロスベル市内の地図作成 2/7
+	#Remove an extra redundant line - both lines are spoken by the same person, and say the same thing.
+	#It is not a mis-set speaker name, because there is a third line that says the same thing again, spoken by the other person.
+	#This could also be changed to remove line 15 instead, if preferred.
+	elif avgID == '12038':
+		delete_frame(script_json, 14)
+
+	#爆弾の捜索（1） 8/13
+	#This scene takes place at the harbor, so change it to use the harbor background
+	elif avgID == '20050':
+		script_json['backgrounds'][0]['id'] = 28
+		
+	#エルベ離宮への荷物配達 4/4
+	#This scene has a footstep sound to indicate that they have moved location/gone back downstairs
+	#So I added an indoor background/bgm to the first half to match the first scene of the quest
+	elif avgID == '12159':
+		insert_schedule(script_json,
+			('backgrounds', {'id': 38, 'start': 1, 'end': 3}),
+			('bgm', {'id': 132, 'start': 1, 'end': 3})
+		)
+
+	#Blue Air 5/7
 	#This one was all kinds of messed up.
 	#It would also be possible to change it to hide the Nacht portrait on line 94,
 	#and interpret all of the dialogue after that as occuring "off-screen".
@@ -126,28 +160,14 @@ def make_edits(script_json, avgID):
 		script_json['dialogueFrames'][98]['expression'] = 12 #expression was unset so this is a guess
 		script_json['dialogueFrames'][98]['CharFadeOut'] = 1
 		script_json['dialogueFrames'][99]['CharFadeOut'] = 0
-
-	#This scene has a footstep sound to indicate that they have moved location/gone back downstairs
-	#So I added an indoor background/bgm to the first half to match the first scene of the quest
-	elif avgID == '12159':
-		insert_schedule(script_json,
-			('backgrounds', {'id': 38, 'start': 1, 'end': 3}),
-			('bgm', {'id': 132, 'start': 1, 'end': 3})
-		)
-		
-	#This scene takes place at the harbor, so change it to use the harbor background
-	elif avgID == '20050':
-		script_json['backgrounds'][0]['id'] = 28
-		
-	#Remove an extra redundant line - both lines are spoken by the same person, and say the same thing.
-	#It is not a mis-set speaker name, because there is a third line that says the same thing again, spoken by the other person.
-	#This could also be changed to remove line 15 instead, if preferred.
-	elif avgID == '12038':
-		delete_frame(script_json, 14)
 	
 #################################################################################
 # Add backgrounds (and maybe music?) to scenes that did not originally have them
 #################################################################################
+
+#Chapter 1
+
+	#爆弾の捜索（1） 9/13
 	#Add a Grancel street background to match the surrounding scenes
 	elif avgID == '20052':
 		insert_schedule(script_json,
